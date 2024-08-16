04b935a7d292b92aac89620e969d00ed2d778f91
alberico <asoares@gdmseeds.com>
2024-08-16 08:31:27 -0300

    feat: add constant color and font values

---
+++ lib/shared/styles.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/cupertino.dart';
+
+const TextStyle heading1Light = TextStyle(
+  fontFamily: 'Inter',
+  fontSize: 93,
+  fontWeight: FontWeight.w300,
+  letterSpacing: 1.5,
+);
+
+const TextStyle heading2Light = TextStyle(
+  fontFamily: 'Inter',
+  fontSize: 58,
+  fontWeight: FontWeight.w300,
+  letterSpacing: 1.5
+);
+
+const TextStyle heading3Regular = TextStyle(
+  fontFamily: 'Inter',
+  fontSize: 46,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 1.5
+);
+
+const TextStyle heading4Regular = TextStyle(
+  fontFamily: 'Inter',
+  fontSize: 33,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 1.5
+);
+
+const TextStyle heading5Regular = TextStyle(
+  fontFamily: 'Inter',
+  fontSize: 23,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 1.5
+);
+
+const TextStyle subtitle1Regular = TextStyle(
+  fontFamily: 'Inter',
+  fontSize: 15,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.15
+);
+
+const TextStyle subtitle2Medium = TextStyle(
+  fontFamily: 'Inter',
+  fontSize: 13,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0.1
+);
+
+const TextStyle paragraph1Regular = TextStyle(
+  fontFamily: 'Inter',
+  fontSize: 15,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.5
+);
+
+const TextStyle paragraph2Medium = TextStyle(
+  fontFamily: 'Inter',
+  fontSize: 13,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.25
+);
+
+const TextStyle label1Semibold = TextStyle(
+  fontFamily: 'Inter',
+  fontSize: 15,
+  fontWeight: FontWeight.w600
+);
+
+const TextStyle label2Regular = TextStyle(
+  fontFamily: 'Inter',
+  fontSize: 10,
+  height: 16,
+  fontWeight: FontWeight.w400
+);
+
+const TextStyle label2Semibold = TextStyle(
+  fontFamily: 'Inter',
+  fontSize: 10,
+  height: 16,
+  fontWeight: FontWeight.w600
+);
+
+const TextStyle button1Bold = TextStyle(
+  fontFamily: 'Inter',
+  fontSize: 16,
+  height: 24,
+  fontWeight: FontWeight.w700
+);
+
+const TextStyle button2Semibold = TextStyle(
+  fontFamily: 'Inter',
+  fontSize: 14,
+  height: 24,
+  fontWeight: FontWeight.w600
+);
+
+const TextStyle button3Semibold = TextStyle(
+  fontFamily: 'Inter',
+  fontSize: 12,
+  height: 24,
+  fontWeight: FontWeight.w600
+);
+
+const TextStyle navbarSmallTitle = TextStyle(
+  fontSize: 18,
+  height: 24,
+  fontWeight: FontWeight.w600,
+);
+
+const TextStyle navbarLargeTitle = TextStyle(
+  fontSize: 34,
+  height: 38,
+  fontWeight: FontWeight.w700,
+);
+
+
+//no
+const TextStyle headlineStyle = TextStyle(
+  fontSize: 30,
+  fontWeight: FontWeight.w700,
+);
+
+const TextStyle bodyStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+);
+
+
+const TextStyle subheadingStyle = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w400,
+);
+
+const TextStyle captionStyle = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+);
