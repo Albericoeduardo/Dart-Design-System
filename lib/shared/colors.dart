7ff54f85f88b3a6ade53c02d870c14937eba3e10
alberico <asoares@gdmseeds.com>
2024-09-06 15:53:29 -0300

    remove useless code

--- lib/shared/colors.dart
+++ lib/shared/colors.dart
@@ -68,9 +68,3 @@ const Color ultrathinMaterialColorDark = Color.fromARGB(112, 42, 42, 42);
 
 //Shadow Colors
 //todo: add shadow colors
-
-//no
-const Color kcPrimaryColor = Color(0xff22A45D);
-const Color kcMediumGreyColor = Color(0xff868686);
-const Color kcLightGreyColor = Color(0xffe5e5e5);
-const Color kcVeryLightGreyColor = Color(0xfff2f2f2);
